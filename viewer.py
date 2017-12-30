1e089da38902b593c29c4e67a16595c886f17954
Andrew Chow <achow101-github@achow101.com>
2017-12-30 01:11:45 -0500

    Display information about the payment request

--- viewer.py
+++ viewer.py
@@ -9,8 +9,31 @@ def print_pr(pr):
     if pr.error:
         print(pr.error)
     else:
-        print(pr.data)
-        print(pr.details)
+        pr.verify()
+        print()
+        print("Payment request data")
+        print("Network: " + pr.details.network)
+        print("Requestor: " + pr.get_requestor())
+        print("Memo: " + pr.get_memo())
+        print("Expiration: " + util.format_time(pr.get_expiration_date()))
+        print("Creation Time: " + util.format_time(pr.details.time))
+        print("Verification Status: " + pr.get_verify_status())
+        print("Merchant Data: " + str(pr.details.merchant_data))
+        print("Outputs:")
+        for out in pr.get_outputs():
+            if out[0] == util.TYPE_ADDRESS:
+                print("  Type: Address")
+                print("  Address: " + out[1])
+            elif out[0] == util.TYPE_PUBKEY:
+                print("  Type: Public Key")
+                print("  Public Key: " + out[1])
+            elif out[0] == util.TYPE_SCRIPT:
+                print("  Type: Script")
+                print("  Script: " + out[1])
+            else:
+                print("  Type: Unknown")
+                print("  Data: " + out[1])
+            print("  Amount: " + util.format_satoshis(out[2]) + " BTC")
 
 if __name__ == '__main__':
     # Command line interface, print welcome message
@@ -19,11 +42,7 @@ if __name__ == '__main__':
     print("Enter a Bitcoin URI:")
     
     for line in fileinput.input():
+        print()
         line = line.strip()
-        try:
-            util.parse_URI(line, print_pr)
-            break
-        except Exception as e:
-            print(e)
-            print("Something failed")
-            break
+        util.parse_URI(line, print_pr)
+        break
